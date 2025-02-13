0be4846156c55af6e545d0ab0eb7a4765f99782a
Titan0605 <titancisneros@gmail.com>
2025-02-13 13:33:09 -0600

    DB modified

--- tech_inventory.sql
+++ tech_inventory.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 13, 2025 at 07:28 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-02-2025 a las 20:32:25
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,50 +18,135 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tech_inventory`
+-- Base de datos: `tech_inventory`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tproduct`
+-- Estructura de tabla para la tabla `tbrands`
 --
 
-CREATE TABLE `tproduct` (
-  `id` int(11) NOT NULL,
-  `sku` varchar(40) DEFAULT NULL,
-  `product_name` varchar(50) DEFAULT NULL,
-  `brand` varchar(40) DEFAULT NULL,
-  `category` varchar(40) DEFAULT NULL,
-  `original_price` double DEFAULT NULL,
-  `discount_price` double DEFAULT NULL,
-  `stock` int(11) DEFAULT NULL,
-  `state` varchar(50) DEFAULT NULL,
-  `location` varchar(50) DEFAULT NULL,
-  `guarantee` int(11) DEFAULT NULL,
-  `last_update` date DEFAULT NULL
+CREATE TABLE `tbrands` (
+  `id_brand` int(11) NOT NULL,
+  `brand_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tcategorys`
+--
+
+CREATE TABLE `tcategorys` (
+  `id_category` int(11) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tlocations`
+--
+
+CREATE TABLE `tlocations` (
+  `id_location` int(11) NOT NULL,
+  `location_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tproducts`
+--
+
+CREATE TABLE `tproducts` (
+  `id_product` int(11) NOT NULL,
+  `id_brand` int(11) NOT NULL,
+  `SKU` varchar(50) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `original_price` double NOT NULL,
+  `discount_price` double NOT NULL,
+  `stock` int(11) NOT NULL,
+  `id_state` int(11) NOT NULL,
+  `id_location` int(11) NOT NULL,
+  `warranty` int(11) NOT NULL,
+  `last_updated` date NOT NULL,
+  `activate` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tstates`
+--
+
+CREATE TABLE `tstates` (
+  `id_state` int(11) NOT NULL,
+  `state_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `tproduct`
+-- Indices de la tabla `tbrands`
 --
-ALTER TABLE `tproduct`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `sku` (`sku`);
+ALTER TABLE `tbrands`
+  ADD PRIMARY KEY (`id_brand`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indices de la tabla `tcategorys`
+--
+ALTER TABLE `tcategorys`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Indices de la tabla `tlocations`
+--
+ALTER TABLE `tlocations`
+  ADD PRIMARY KEY (`id_location`);
+
+--
+-- Indices de la tabla `tproducts`
+--
+ALTER TABLE `tproducts`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_brand` (`id_brand`),
+  ADD KEY `id_category` (`id_category`),
+  ADD KEY `id_state` (`id_state`),
+  ADD KEY `id_location` (`id_location`);
+
+--
+-- Indices de la tabla `tstates`
+--
+ALTER TABLE `tstates`
+  ADD PRIMARY KEY (`id_state`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `tproduct`
+-- AUTO_INCREMENT de la tabla `tproducts`
 --
-ALTER TABLE `tproduct`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tproducts`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tproducts`
+--
+ALTER TABLE `tproducts`
+  ADD CONSTRAINT `tproducts_ibfk_1` FOREIGN KEY (`id_brand`) REFERENCES `tbrands` (`id_brand`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tproducts_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `tcategorys` (`id_category`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tproducts_ibfk_3` FOREIGN KEY (`id_state`) REFERENCES `tstates` (`id_state`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tproducts_ibfk_4` FOREIGN KEY (`id_location`) REFERENCES `tlocations` (`id_location`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
